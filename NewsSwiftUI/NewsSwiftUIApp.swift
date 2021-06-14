4433f0a3ce8693078ebd556b9364f55cda297d15
Gary Hanson <hcanfly@gmail.com>
2021-06-13 17:11:00 -0700

    Initial Commit

---
+++ NewsSwiftUI/NewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSwiftUIApp.swift
+//  NewsSwiftUI
+//
+//  Created by Gary Hanson on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
